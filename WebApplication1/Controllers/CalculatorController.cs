f43369b2f018d4464474e7a88a887c6c8e542f3e
manish.bharti <manish.bharti@IATPL-PC2>
2025-02-17 16:37:25 +0530

    Add project files.

---
+++ WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Calculate(string operation, double num1, double num2)
+        {
+            double result = 0;
+            switch (operation)
+            {
+                case "add":
+                    result = num1 + num2;
+                    break;
+                case "subtract":
+                    result = num1 - num2;
+                    break;
+                case "multiply":
+                    result = num1 * num2;
+                    break;
+                case "divide":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "Cannot divide by zero.";
+                        return View("Index");
+                    }
+                    break;
+                default:
+                    ViewBag.ErrorMessage = "Invalid operation.";
+                    return View("Index");
+            }
+
+            ViewBag.Result = result;
+            return View("Index");
+        }
+    }
+}
---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+       
+        private const string HardcodedUsername = "admin";
+        private const string HardcodedPassword = "password123";
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (model.Username == HardcodedUsername && model.Password == HardcodedPassword)
+            {
+               
+                return RedirectToAction("Index", "Calculator");
+            }
+
+            
+            ViewBag.ErrorMessage = "Invalid username or password.";
+            return View("Index");
+        }
+    }
+}
